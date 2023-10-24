a0917c17b21df9d9f9e503835e1cf1658dac4081
Molow223 <molow223@gmail.com>
2023-10-24 20:07:41 +0500

    portfolio 2023

--- js.js
+++ js.js
@@ -4,62 +4,70 @@ var colorArray = ["#5A9C6E", "#A8BF5A", "#FAC46E", "#FAD5BB", "#549DA0"];
 var i = 0;
 
 function changeColor() {
-    document.getElementById("work").style.backgroundColor = colorArray[i];
-    i++;
-    if (i > colorArray.length - 1) {
-        i = 0;
-    }
+
+  document.getElementById("body").style.backgroundColor = colorArray[i];
+  i++;
+  if (i > colorArray.length - 1) {
+    i = 0;
+  }
 };
 
 $(document).ready(function () {
-    $('.header__burger').click(function (event) {
-        $('.header__burger,.header__menu').toggleClass('active');
-    });
+  $('.header__burger').click(function (event) {
+    $('.header__burger,.header__menu').toggleClass('active');
+  });
 });
+//about
+const upBtn = document.querySelector('.up-button')
+const downBtn = document.querySelector('.down-button')
+const sidebar = document.querySelector('.sidebar')
+const mainSlide = document.querySelector('.main-slide')
+const container = document.querySelector('.container')
+const slidesCount = mainSlide.querySelectorAll('div').length
+let activeSlideIndex = 0;
 
+sidebar.style.top = `-${(slidesCount-1)*100}vh`
 
-let btnLeft = document.querySelector(".btnLeft");
-let btnRight = document.querySelector(".btnRight");
-let slides = document.querySelectorAll("img");
-
-btnRight.addEventListener("click", function () {
-    // Увеличиваем переменную i
-    i++
-    // Условие если переменная i больше или равна количеству слайдов
-    if (i >= slides.length) {
-        // Удаляем класс block предыдущему слайду
-        slides[i - 1].classList.remove("block");
-        // Присваиваем переменной i ноль
-        i = 0;
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-    } else { // Иначе
-        // Удаляем класс block предыдущему слайду
-        slides[i - 1].classList.remove("block");
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-    }
-
+upBtn.addEventListener('click', ()=>{
+    changeSlide('up')
 })
-btnLeft.addEventListener("click", function () {
-    // Уменьшаем переменную i
-    --i
-  // Условие если переменная i больше или равна количеству слайдов
-    if (i >= slides.length) {
-        // Удаляем класс block следующему слайду
-        slides[i + 1].classList.remove("block");
-        // Присваиваем переменной i ноль
-        i = 0;
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-    } else { // Иначе
-        // Удаляем класс block предыдущему слайду
-        slides[i + 1].classList.remove("block");
-        // Добавляем класс block следующему слайду
-        slides[i].classList.add("block");
-    }
+downBtn.addEventListener('click', ()=>{
+    changeSlide('down')
 })
+function changeSlide(direction) {
+    if(direction === 'up') {
+        activeSlideIndex++
+        if(activeSlideIndex === slidesCount) {
+            activeSlideIndex =0
+        }
+    } else if(direction === 'down') {
+        activeSlideIndex--
+        if(activeSlideIndex < 0) {
+            activeSlideIndex = slidesCount - 1
+        }
+    }
+    const height = container.clientHeight
 
+    mainSlide.style.transform = `translateY(-${activeSlideIndex * height}px)`
+    sidebar.style.transform = `translateY(${activeSlideIndex * height}px)`
+
+}
+//Skills
+const slides = document.querySelectorAll('.slide')
+
+for (const slide of slides) {
+    slide.addEventListener('click', () => {
+        clearActiveClasses()
+
+        slide.classList.add('active')
+    })
+
+    function clearActiveClasses() {
+        slides.forEach((slide) => {
+            slide.classList.remove('active')
+        })
+    }
+}
 
 // Create a "close" button and append it to each list item
 var myNodelist = document.getElementsByTagName("LI");
@@ -76,7 +84,7 @@ for (i = 0; i < myNodelist.length; i++) {
 var close = document.getElementsByClassName("close");
 var i;
 for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
+  close[i].onclick = function () {
     var div = this.parentElement;
     div.style.display = "none";
   }
@@ -84,7 +92,7 @@ for (i = 0; i < close.length; i++) {
 
 // Add a "checked" symbol when clicking on a list item
 var list = document.querySelector('#to_do__ul');
-list.addEventListener('click', function(ev) {
+list.addEventListener('click', function (ev) {
   if (ev.target.tagName === 'LI') {
     ev.target.classList.toggle('checked');
   }
@@ -110,7 +118,7 @@ function newElement() {
   li.appendChild(span);
 
   for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
+    close[i].onclick = function () {
       var div = this.parentElement;
       div.style.display = "none";
     }
@@ -119,42 +127,42 @@ function newElement() {
 
 
 function Calendar2(id, year, month) {
-  var Dlast = new Date(year,month+1,0).getDate(),
-      D = new Date(year,month,Dlast),
-      DNlast = new Date(D.getFullYear(),D.getMonth(),Dlast).getDay(),
-      DNfirst = new Date(D.getFullYear(),D.getMonth(),1).getDay(),
-      calendar = '<tr>',
-      month=["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+  var Dlast = new Date(year, month + 1, 0).getDate(),
+    D = new Date(year, month, Dlast),
+    DNlast = new Date(D.getFullYear(), D.getMonth(), Dlast).getDay(),
+    DNfirst = new Date(D.getFullYear(), D.getMonth(), 1).getDay(),
+    calendar = '<tr>',
+    month = ["Январь", "Февраль", "Март", "Апрель", "Май", "Июнь", "Июль", "Август", "Сентябрь", "Октябрь", "Ноябрь", "Декабрь"];
   if (DNfirst != 0) {
-    for(var  i = 1; i < DNfirst; i++) calendar += '<td>';
-  }else{
-    for(var  i = 0; i < 6; i++) calendar += '<td>';
+    for (var i = 1; i < DNfirst; i++) calendar += '<td>';
+  } else {
+    for (var i = 0; i < 6; i++) calendar += '<td>';
   }
-  for(var  i = 1; i <= Dlast; i++) {
+  for (var i = 1; i <= Dlast; i++) {
     if (i == new Date().getDate() && D.getFullYear() == new Date().getFullYear() && D.getMonth() == new Date().getMonth()) {
       calendar += '<td class="today">' + i;
-    }else{
+    } else {
       calendar += '<td>' + i;
     }
-    if (new Date(D.getFullYear(),D.getMonth(),i).getDay() == 0) {
+    if (new Date(D.getFullYear(), D.getMonth(), i).getDay() == 0) {
       calendar += '<tr>';
     }
   }
-  for(var  i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
-  document.querySelector('#'+id+' tbody').innerHTML = calendar;
-  document.querySelector('#'+id+' thead td:nth-child(2)').innerHTML = month[D.getMonth()] +' '+ D.getFullYear();
-  document.querySelector('#'+id+' thead td:nth-child(2)').dataset.month = D.getMonth();
-  document.querySelector('#'+id+' thead td:nth-child(2)').dataset.year = D.getFullYear();
-  if (document.querySelectorAll('#'+id+' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
-      document.querySelector('#'+id+' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
+  for (var i = DNlast; i < 7; i++) calendar += '<td>&nbsp;';
+  document.querySelector('#' + id + ' tbody').innerHTML = calendar;
+  document.querySelector('#' + id + ' thead td:nth-child(2)').innerHTML = month[D.getMonth()] + ' ' + D.getFullYear();
+  document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.month = D.getMonth();
+  document.querySelector('#' + id + ' thead td:nth-child(2)').dataset.year = D.getFullYear();
+  if (document.querySelectorAll('#' + id + ' tbody tr').length < 6) {  // чтобы при перелистывании месяцев не "подпрыгивала" вся страница, добавляется ряд пустых клеток. Итог: всегда 6 строк для цифр
+    document.querySelector('#' + id + ' tbody').innerHTML += '<tr><td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;<td>&nbsp;';
   }
-  }
-  Calendar2("calendar2", new Date().getFullYear(), new Date().getMonth());
-  // переключатель минус месяц
-  document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(1)').onclick = function() {
-    Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)-1);
-  }
-  // переключатель плюс месяц
-  document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function() {
-    Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month)+1);
-  } 
+}
+Calendar2("calendar2", new Date().getFullYear(), new Date().getMonth());
+// переключатель минус месяц
+document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(1)').onclick = function () {
+  Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month) - 1);
+}
+// переключатель плюс месяц
+document.querySelector('#calendar2 thead tr:nth-child(1) td:nth-child(3)').onclick = function () {
+  Calendar2("calendar2", document.querySelector('#calendar2 thead td:nth-child(2)').dataset.year, parseFloat(document.querySelector('#calendar2 thead td:nth-child(2)').dataset.month) + 1);
+} 
